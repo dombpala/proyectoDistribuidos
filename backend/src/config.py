d9774b664694f8d0f1dd1f7d17d8acc747c901bd
root <root@ip-172-31-90-116.ec2.internal>
2022-01-18 04:37:00 +0000

    correcting docker-compose file

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,5 +1,5 @@
 import os	
 PWD = os.path.abspath(os.curdir)	
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/gestion_mascotas'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:dombpala_YUGIOHGX99@localhost/gestion_mascotas'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
